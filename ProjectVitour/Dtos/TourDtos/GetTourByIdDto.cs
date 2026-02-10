534583a83836c5b9a6f2dd225570dedd93aa5116
Yiğit Öner <yigit.oner@outlook.com.tr>
2026-02-11 00:54:52 +0300

    Add project files.

---
+++ ProjectVitour/Dtos/TourDtos/GetTourByIdDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectVitour.Dtos.TourDtos
+{
+    public class GetTourByIdDto
+    {
+        public string TourID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CoverImageUrl { get; set; }
+        public string Badge { get; set; }
+        public int DayCount { get; set; }
+        public int Capacity { get; set; }
+        public decimal Price { get; set; }
+        public bool IsStatus { get; set; }
+    }
+}
---
+++ ProjectVitour/Settings/IDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectVitour.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string TourCollectionName { get; set; }
+    }
+}
